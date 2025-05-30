abc2144599a45cae81db29cb518dd6678582d2a5
Stulionus <mcapcanari@gmail.com>
2025-05-29 20:02:52 -0700

    added model, new arguments in grid, cells in grid is not 2D

--- grid.py
+++ grid.py
@@ -7,24 +7,33 @@ from cell import Cell
 #from cars import car
 
 class Grid:
-    def __init__(self):
-        self.cells = []
-        width = 200
-        height = 200
+    def __init__(self, 
+                 width, 
+                 height, 
+                 road_remove_probability = 0.1, 
+                 event_chance = 0.1, 
+                 cars_prob= 0.1):
+        
+
+        self.cells = [[None for _ in range(width)]for _ in range(height)]
+        
+        self.width = width
+        self.height =  height
         block_density = (10, 30)
         base_road_width = 2
         wide_road_width = 4
         highway_width = 6
-        road_remove_probability = 0.2
+        self.road_remove_probability = road_remove_probability
+        self.even_chance = event_chance
 
         self.city = City(
-            width=width,
-            height=height,
+            width=self.width,
+            height=self.height,
             block_size_range=block_density,
             base_road_width=base_road_width,
             wide_road_width=wide_road_width,
             highway_width=highway_width,
-            road_remove=road_remove_probability
+            road_remove=self.road_remove_probability
         )
 
         self.city.generateRoads()
@@ -35,27 +44,29 @@ class Grid:
         for cell in self.cells:
             if cell.cell_type == "road":
                 #add slight chance of spawning car
-                cars.append(spawnCar(cell.x, cell.y))
+                if np.random.rand() < cars_prob:
+                    cars.append(spawnCar(cell.x, cell.y))
 
     def roadsToGrid(self, city):
-        currentGrid = city.grid
-        intersections = city.intersections
-        horizontal = city.horizontal_roads
-        vertical = city.vertical_roads
+        for y in range(self.height):
+            for x in range(self.width):
+                if city.grid[y,x] == -1:
+                    continue
 
-        for y in range(currentGrid.shape[0]):
-            for x in range(currentGrid.shape[1]):
-                if currentGrid[y, x] == -1:
-                    continue  # Not a road
+                c = Cell(x, y, city.grid[y, x])
+                c.addPossibleMoves(city,
+                                   city.intersections,
+                                   city.horizontal_roads,
+                                   city.vertical_roads)
 
-                c = Cell(x, y, currentGrid[y, x])
-                c.addPossibleMoves(city, intersections, horizontal, vertical)
-                self.cells.append(c)
+                self.cells[y][x] = c
 
     def add_Random_events(self, event_chance = 0.1):
         for c in self.cells:
             if c.cell_type == "road" and np.random.rand() <  event_chance:
                 c.cell_type = "non-road"
                 self.city.grid[c.y, c.x] = -1
+
+
 
         
---
+++ model.py
@@ -0,0 +1,38 @@
+import numpy as np
+import grid as grid
+
+class Model:
+    def __init__(self, 
+                 height, 
+                 width,
+                 time, 
+                 cars_prob, 
+                 road_remove_probability = 0.1,
+                 event_chance = 0.1,
+                 traffic_light_time = 10,
+                 move_chance = 0.9,
+                 ):
+        
+        self.cars_prob = cars_prob
+        self.time = time
+        self.traffic_light_time = traffic_light_time
+        self.move_chance = move_chance
+        self.event_chance = event_chance
+
+    def make_grid(self):
+        self.grid = grid(
+            width = self.width,
+            height = self.height,    
+            road_remove_probability = self.road_remove_probability,
+            event_chance = self.event_chance,
+            cars_prob = self.cars_prob,
+        )
+
+    def simulate(self):
+        for i in range(self.time):
+            self.grid.update()
+
+    def simulate_w_plot(self):
+        for i in range(self.time):
+            self.grid.update()
+            #Visualize
