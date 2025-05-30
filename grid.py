7a6641f0de638b16da5fb8d515e39d6494e92297
Cayman <120585887+BagWithoutTheR@users.noreply.github.com>
2025-05-29 17:24:48 -0700

    Update grid.py
    fixing

--- grid.py
+++ grid.py
@@ -1,73 +1,55 @@
-class Cell:
-    def __init__(self, x, y, cell_type):
-        self.x = x
-        self.y = y
-        self.cell_type = cell_type
-        self.canMove = []
+import numpy as np
+import numpy.ma as ma
+import matplotlib.pyplot as plt
 
-    def addMove(self, move):
-        self.canMove.append(move)
+from roads import City
+from cell import Cell
+#from cars import car
 
-    def getPossibleMoves(self):
-        return self.canMove
+class Grid:
+    def __init__(self):
+        self.cells = []
+        width = 200
+        height = 200
+        block_density = (10, 30)
+        base_road_width = 2
+        wide_road_width = 4
+        highway_width = 6
+        road_remove_probability = 0.2
 
-    def addPossibleMoves(self, city, intersections, horizontal, vertical):
-        x, y = self.x, self.y
-        grid = city.grid
+        self.city = City(
+            width=width,
+            height=height,
+            block_size_range=block_density,
+            base_road_width=base_road_width,
+            wide_road_width=wide_road_width,
+            highway_width=highway_width,
+            road_remove=road_remove_probability
+        )
 
-        def is_valid(x, y):
-            return 0 <= x < grid.shape[1] and 0 <= y < grid.shape[0]
+        self.city.generateRoads()
 
-        def is_intersection(x, y):
-            return is_valid(x, y) and intersections[y, x]
+        self.roadsToGrid(self.city)
 
-        def is_road(x, y):
-            return is_valid(x, y) and (horizontal[y, x] or vertical[y, x])
+        cars = []
+        for cell in self.cells:
+            if cell.cell_type == "road":
+                #add slight chance of spawning car
+                spawnCar(cell.x, cell.y)
 
-        # Intersection logic
-        if is_intersection(x, y):
-            neighbors = {
-                "N": is_road(x, y - 1),
-                "S": is_road(x, y + 1),
-                "E": is_road(x + 1, y),
-                "W": is_road(x - 1, y),
-                "SE": is_road(x + 1, y + 1),
-                "SW": is_road(x - 1, y + 1),
-                "NE": is_road(x + 1, y - 1),
-                "NW": is_road(x - 1, y - 1),
-            }
+    def roadsToGrid(self, city):
+        currentGrid = city.grid
+        intersections = city.intersections
+        horizontal = city.horizontal_roads
+        vertical = city.vertical_roads
 
-            # Determine intersection corner
-            if not neighbors["SE"]:
-                if neighbors["N"]: self.addMove((0, -1))  # N
-                if neighbors["E"]: self.addMove((1, 0))   # E
-            elif not neighbors["SW"]:
-                if neighbors["N"]: self.addMove((0, -1))  # N
-                if neighbors["W"]: self.addMove((-1, 0))  # W
-            elif not neighbors["NW"]:
-                if neighbors["S"]: self.addMove((0, 1))   # S
-                if neighbors["W"]: self.addMove((-1, 0))  # W
-            elif not neighbors["NE"]:
-                if neighbors["S"]: self.addMove((0, 1))   # S
-                if neighbors["E"]: self.addMove((1, 0))   # E
+        for y in range(currentGrid.shape[0]):
+            for x in range(currentGrid.shape[1]):
+                if currentGrid[y, x] == -1:
+                    continue  # Not a road
 
-        # Horizontal or vertical road logic
-        elif is_road(x, y):
-            NW = is_road(x - 1, y - 1)
-            N  = is_road(x, y - 1)
-            NE = is_road(x + 1, y - 1)
-            W  = is_road(x - 1, y)
-            E  = is_road(x + 1, y)
-            SW = is_road(x - 1, y + 1)
-            S  = is_road(x, y + 1)
-            SE = is_road(x + 1, y + 1)
+                c = Cell(x, y, currentGrid[y, x])
+                c.addPossibleMoves(city, intersections, horizontal, vertical)
+                self.cells.append(c)
 
-            # Right side detection
-            if not (NW or N or NE) and (W and E and SW and S and SE):
-                self.addMove((-1, 0))  # top side of horizontal, go west
-            elif not (SW or S or SE) and (W and E and NW and N and NE):
-                self.addMove((1, 0))   # bottom side of horizontal, go east
-            elif not (NE or E or SE) and (N and S and NW and W and SW):
-                self.addMove((0, 1))   # left side of vertical, go south
-            elif not (NW or W or SW) and (N and S and NE and E and SE):
-                self.addMove((0, -1))  # right side of vertical, go north
+        
