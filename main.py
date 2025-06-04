9ed2016b578f6885236179bd48e914748334fdab
Cayman <120585887+BagWithoutTheR@users.noreply.github.com>
2025-06-04 00:35:17 -0700

    Update main.py
    Updated amoutn of medium roads and non working high way amount

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
     base_road_width = 2
     wide_road_width = 4
     highway_width = 6
+    highway_amount = 2
+    medium_road_amount = 7
     road_remove_probability = 0.2
 
     time_steps = 200
@@ -17,6 +19,8 @@ def main():
     model = Model(
         width=width,
         height=height,
+        highway_amount=highway_amount,
+        medium_road_amount=medium_road_amount,
         time=time_steps, 
         road_remove_probability=road_remove_probability,
         event_chance=0.1,
@@ -30,20 +34,20 @@ def main():
     t2 = time.perf_counter()
     # model.simulate_w_plot()
     sim_length = 100
-    for i in range(sim_length):
-        model.simulate()
-        model.grid.reset_cars()
-        print(f"Simulating: {i/sim_length*100:.2f}%", end="\r", flush=True)
-    t3 = time.perf_counter()
-    print(f"Simulation (with plotting) took {t3 - t2:.4f} seconds")
+    ##for i in range(sim_length):
+    #    model.simulate()
+    #    model.grid.reset_cars()
+    #    print(f"Simulating: {i/sim_length*100:.2f}%", end="\r", flush=True)
+    #t3 = time.perf_counter()
+    #print(f"Simulation (with plotting) took {t3 - t2:.4f} seconds")
 
-    model.plot_traffic_heatmap()
-    model.plot_car_count_heatmap()
+    #model.plot_traffic_heatmap()
+    #model.plot_car_count_heatmap()
     # t4 = time.perf_counter()
     # model.grid.reset_cars()
     # t5 = time.perf_counter()
     # print(f"Car reset took {t5 - t4:.4f} seconds")
-    # model.simulate_w_plot()
+    model.simulate_w_plot()
 
 if __name__ == "__main__":
     main()
